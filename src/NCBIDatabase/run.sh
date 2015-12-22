8ad6f2eb0d211c43b7bf02e6e047b5a1d43095bf
dkoslicki <dmkoslicki@gmail.com>
2015-12-21 23:23:39 -0800

    typo fix

--- src/NCBIDatabase/run.sh
+++ src/NCBIDatabase/run.sh
@@ -2,8 +2,8 @@
 outDir="out"
 nProcs=15
 
-#python generate_taxonomy.py --output ${outDir}/taxonomy.txt --output_red ${outDir}/taxonomy_reduced.txt --pickle ${outDir}/taxonomy.pkl | tee ${outDir}/generate_taxonomy.txt
-#python repophlan_get_microbes.py --taxonomy ${outDir}/taxonomy_reduced.txt --out_dir ${outDir}/microbes --nproc ${nProcs} --out_summary ${outDir}/repophlan_microbes.txt | tee ${outDir}/repophlan_microbes.log
+python generate_taxonomy.py --output ${outDir}/taxonomy.txt --output_red ${outDir}/taxonomy_reduced.txt --pickle ${outDir}/taxonomy.pkl | tee ${outDir}/generate_taxonomy.txt
+python repophlan_get_microbes.py --taxonomy ${outDir}/taxonomy_reduced.txt --out_dir ${outDir}/microbes --nproc ${nProcs} --out_summary ${outDir}/repophlan_microbes.txt | tee ${outDir}/repophlan_microbes.log
 python generate_taxonomy_taxid.py --output ${outDir}/taxonomy_taxID.txt --output_red ${outDir}/taxonomy_reduced_taxID.txt --pickle ${outDir}/taxonomy_taxID.pkl | tee ${outDir}/generate_taxonomy_tax_ID.txt
-#python repophlan_get_viruses.py --taxonomy ${outDir}/taxonomy_reduced_taxID.txt --out_dir ${outDir}/viruses --out_summary ${outDir}/repophlan_viruses.txt | tee ${outDir}/repophlan_viruses.log
+python repophlan_get_viruses.py --taxonomy ${outDir}/taxonomy_reduced_taxID.txt --out_dir ${outDir}/viruses --out_summary ${outDir}/repophlan_viruses.txt | tee ${outDir}/repophlan_viruses.log
 python parse_taxonomy.py --taxonomy ${outDir}/taxonomy_reduced_taxID.txt --out_dir ${outDir} --repophlan_dir ${outDir}
