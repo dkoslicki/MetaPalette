e4aa06d1be166b3a767ce9263df1d41b0c412e53
dkoslicki <dmkoslicki@gmail.com>
2015-12-22 09:42:36 -0800

    taxid column moved

--- src/NCBIDatabase/parse_taxonomy.py
+++ src/NCBIDatabase/parse_taxonomy.py
@@ -26,8 +26,9 @@ if __name__ == '__main__':
 			pass
 		else:
 			Accession = line.strip().split('\t')[0]
-			TaxID = line.strip().split('\t')[26]
-			filename = line.strip().split('\t')[12]
+			TaxID = line.strip().split('\t')[25]
+			#filename = line.strip().split('\t')[12]
+			#filename = os.path.join(fna_dir,accession+".fna.bz2")
 			Accession2TaxID[Accession] = TaxID
 		i=1
 	fid.close()
