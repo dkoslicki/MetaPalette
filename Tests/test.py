84dfc8faf815c3ab1d4ce102da23c2ef0b75292a
dkoslicki <dmkoslicki@gmail.com>
2016-01-20 00:12:01 -0800

    update test script

--- Tests/test.py
+++ Tests/test.py
@@ -1,10 +1,10 @@
 #This will run each of the main scripts of MetaPalette and check the results with the pre-computed values
 
-python ../src/Python/Train.py -i Data/FullFileNames.txt -o TestOutput -b bcalm -r Data -j jellyfish -c ../src/CountInFile/./count_in_file -t 4 -k 5
+python ../src/Python/Train.py -i Data/FullFileNames.txt -o TestOutput -b bcalm -r `pwd` -j jellyfish -c count_in_file -t 4 -k 5 -s 4
 
 #Write code to check if output makes sense
 
-python ../src/Python/Classify.py -d TestOutput -o TestOutput -i Data/test-reads.fa -k default -j jellyfish -q ../src/QueryPerSeq/./query_per_sequence -Q C -y -x -t 4
+python ../src/Python/Classify.py -d TestOutput -o TestOutput -i Data/test-reads.fa -k default -j jellyfish -q query_per_sequence -Q C -y -x -t 4
 
 #Write code to check if output makes sense
 
